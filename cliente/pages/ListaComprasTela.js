826d95ff6d9fa98a8494cf7a3d5e99d0ed349a2f
Caio Rodrigo <rodrigocaio2775@gmail.com>
2023-11-29 17:05:17 -0300

    IMPROVEMENT - ajustes de cors e API

--- cliente/pages/ListaComprasTela.js
+++ cliente/pages/ListaComprasTela.js
@@ -1,29 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
-import api from '../services/api';
-import React, { useState,  useEffect  } from 'react'
-
-
-
-
+import { View, Text, StyleSheet } from "react-native";
+import api from "../services/api";
+import React, { useState, useEffect } from "react";
 
 const ListaComprasScreen = () => {
-
   const [compras, setCompras] = useState([]);
 
-useEffect( () => {
-  
-  const listarCompras = async () => {
-    const response = await api.get();
-    setCompras(response.data);
-  }
-
-  listarCompras();
-}, []);
-
+  useEffect(() => {
+    const listarCompras = async () => {
+      try{
+        const response = await api.get();
+        setCompras(response.data);
+      } catch (erro) {
+        console.error('Erro:', erro);
+      }
+    };
+    
+    listarCompras();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style ={styles.text}>Lista de Compras</Text>
+      <Text style={styles.text}>Lista de Compras</Text>
       <Text> {JSON.stringify(compras)} </Text>
       {/* Adicione aqui a lógica para exibir a lista de compras */}
     </View>
@@ -31,16 +28,16 @@ useEffect( () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'start',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    text: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginTop: 10
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: "start",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+});
 export default ListaComprasScreen;
