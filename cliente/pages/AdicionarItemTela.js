9c5629356c85dbede0ea1876a8d9d1e776fdbea4
Caio Rodrigo <rodrigocaio2775@gmail.com>
2023-11-30 07:49:24 -0300

    Aplica método post nas telas

--- cliente/pages/AdicionarItemTela.js
+++ cliente/pages/AdicionarItemTela.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import api from '../services/api';
 
 const AdicionarItemScreen = () => {
+
+  const [ultimoId, setUltimo] = useState(0);
+  const [descricao, setDesc] = useState("");
+  const [comprado, setComp] = useState(false);
+
+  useEffect( () => {
+  
+    const listarCompras = async () => {
+      const response = await api.get().catch(console.log);
+      response.data.sort((a, b) => b.id - a.id);
+      setUltimo(response.data[0].id);
+    }
+  
+    listarCompras();
+  }, []);
+
+  function adicionar(compra) {
+    try{
+      api.post("",compra);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Carrinho</Text>
