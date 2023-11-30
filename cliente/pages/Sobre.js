904fa2d11b8c7c35edf0d8b8412e2f7e0d90e8d7
Gustavo Fernandes Reis <93928976+gureispt@users.noreply.github.com>
2023-11-30 10:06:50 -0300

    CRUD - mudanças na lógica

--- cliente/pages/Sobre.js
+++ cliente/pages/Sobre.js
@@ -4,11 +4,8 @@ import { View, Text, StyleSheet } from "react-native";
 const SobreScreen = () => {
   return (
     <View style={styles.container}>
-        <Text>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+        <Text style={styles.text}>
+            Esse projeto foi densenvolvido por Gustavo Fernandes Reis e Caio Rodrigo  
         </Text>
       {/* Adicione aqui a lógica para exibir a lista de compras */}
     </View>
@@ -24,8 +21,10 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 10,
+    fontFamily: 'Times New Roman',
+    margin: 10,
+    alignItems: 'center',
+
   },
 });
 
--- servidor/index.js
+++ servidor/index.js
@@ -20,14 +20,16 @@ const compras = [];
 // CREATE 
 server.post('/compras' , (req, res) => {
     const compra = req.body;
-    console.log(req.body)
     compras.forEach(c => {
         if(c.id == compra.id){
             res.errored.message = "Já existe uma compra com esse id";
             return res.errored;
         } 
     })
-    compras.push(compra);
+    compra.forEach(c => {
+        compras.push(c);
+    })
+    
     return res.json(compras);
 });
 
@@ -36,7 +38,7 @@ server.get('/compras/:id', (req, res) => {
     const {id} = req.params;
 
     compras.forEach(compra => {
-        if(compra.id == id) return res.json(compra);
+        if(compras.indexOf(compra) == id) return res.json(compra);
     })
     return res.json();
     
@@ -49,20 +51,25 @@ server.get('/compras', (req, res) => {
 
 // UPDATE
 server.put('/compras/:id', (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const compra = req.body;
 
-    compras.forEach((c,i) => {
-        if(c.id == id){
-            compras[i] = compra
-            console.log(c)
-        } else {
-            res.errored.message = 'Compra não encontrada';
-            return res.errored;
+    let compraEncontrada = false;
+
+    compras.forEach((c, i) => {
+        if (c.id == id) {
+            compras[i] = compra;
+            compraEncontrada = true;
+            
         }
-    })
-    return res.json(compras);
+    });
+    if (!compraEncontrada) {
+        res.status(404).json({ message: 'Compra não encontrada' });
+    } else {
+        res.json(compras);
+    }
 });
+
 
 // DELETE
 server.delete('/compras/:id', (req, res) => {
@@ -70,14 +77,14 @@ server.delete('/compras/:id', (req, res) => {
     compras.forEach((c, i) => {
         if(c.id == id){
             compras.splice(i, 1);
-        } else {
-            res.errored.message = 'Compra não encontrada';
-            return res.errored;
+            compraEncontrada = true;
         }
     })
+    if (!compraEncontrada) {
+        res.status(404).json({ message: 'Compra não encontrada' });
+    } else {
+        res.json(compras);
+    }
     return res.json({message: 'O item foi deletado'});
-});
-
-
-
+}); 
 server.listen(3000, () => console.log("Servidor rodando!"));
