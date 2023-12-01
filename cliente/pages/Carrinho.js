b717813cce896d0ca959b5dc4aad5c5f3e43fb11
Gustavo Fernandes Reis <93928976+gureispt@users.noreply.github.com>
2023-12-01 10:13:23 -0300

    CARRINHO - Não dei pull antes de fazer mudanças

--- cliente/pages/Carrinho.js
+++ cliente/pages/Carrinho.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput, StyleSheet, Button} from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  Text,
+  ScrollView,
+} from "react-native";
 import api from "../services/api";
+import { Ionicons } from "@expo/vector-icons";
 
 const AdicionarItemScreen = () => {
   const [ultimoId, setUltimo] = useState(0);
   const [descricao, setDesc] = useState("");
   const [comprado, setComp] = useState(false);
+  const [compras, setCompras] = useState([]);
 
   useEffect(() => {
     const listarCompras = async () => {
       const response = await api.get();
       response.data.sort((a, b) => b.id - a.id);
       setUltimo(response.data[0]?.id);
+      setCompras(response.data)
     };
 
     listarCompras();
   }, []);
 
-  function adicionar(compra) {
+  const adicionar = (compra) => {
     try {
       api.post("", compra);
     } catch (err) {
@@ -25,9 +35,23 @@ const AdicionarItemScreen = () => {
     }
   }
 
+  const renderCompra = (compra) => {
+    // if(compra.comprado){
+    //   return (
+    //     <View style></View>
+    //   )
+    // }
+      return (<View>
+        <Text style={{color:'#fff'}}> {compra.descricao}</Text>
+        
+        {/* <Ionicons name="delete" size={size} color={color} /> */}
+      </View>)
+    
+  }
+
   return (
     <View style={styles.container}>
-      <View style={{width: '80%', alignSelf: 'center'}}>
+      <View style={{ width: "80%", alignSelf: "center" }}>
         <TextInput
           style={{
             height: 40,
@@ -48,6 +72,13 @@ const AdicionarItemScreen = () => {
             setUltimo(ultimoId + 1);
           }}
         />
+        
+        <View style={{ marginTop: 10 }}>
+        <ScrollView style={{paddingHorizontal: 20}}>
+          {compras.map((compra) => renderCompra(compra))}
+        </ScrollView>
+
+      </View>
       </View>
     </View>
   );
